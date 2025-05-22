ba984533468ad489c930a90e73f8c72564163fa1
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-21 21:50:51 -0300

    js test

---
+++ AULA-JS/scripts/app.js
@@ -0,0 +1,6 @@
+console.log("Hello World, dentro do app.js");
+
+let nome;  //Cria uma variável com valor indefinido
+let disciplina = null;  //Cria uma variável vazia
+let texto = "Este é um exemplo";  //Cria uma variável com um valor
+
