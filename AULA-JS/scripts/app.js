b42edaf757039f1d00f062971ed205e1dfd29196
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-21 22:25:49 -0300

    console.log, typeof, const, let, var, if/else

--- AULA-JS/scripts/app.js
+++ AULA-JS/scripts/app.js
@@ -2,24 +2,36 @@ let nome = "johnny";
 var sobrenome = "dias mathias";  // Não utilizar var, pois pode sobrescrever variáveis
 const ultimoNome = "junior";
 
-const idade = 18;5;
+const idade =  18;
+const idade2 = "18"; // number = string
+
+const valor1 = "";
+const valor2 = false; //false e null são iguais
+
+const valor3 = "1";
+const valor4 = true; // 1 e true são iguais
 
 let mensagem;
 
 if (idade < 18) {
-
     mensagem = "é menor de idade";
-
-}
-else{
-
+} else if (idade == 18) {
+    mensagem = "tem exatamente 18 anos";
+}else{
     mensagem = "é maior de idade";
-    
 }
 
 console.log(nome, sobrenome, ultimoNome);
 
 console.log('Tipo da variavel idade', typeof idade);
 
-console.log(nome, mensagem);
+console.log(nome, idade, mensagem);
+
+if (idade == idade2) {
+    console.log("são iguais");
+} else {
+    console.log("não são iguais");
+}
+
+
 
