02931462fe244ed92b1f4d8295aae9a42f8b78cf
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-21 22:43:07 -0300

    function

--- AULA-JS/scripts/app.js
+++ AULA-JS/scripts/app.js
@@ -1,37 +1,10 @@
-let nome = "johnny";
-var sobrenome = "dias mathias";  // Não utilizar var, pois pode sobrescrever variáveis
-const ultimoNome = "junior";
+document.getElementById("toggle").addEventListener('click', function() {
+    const info = document.getElementById("info");
 
-const idade =  18;
-const idade2 = "18"; // number = string
-
-const valor1 = "";
-const valor2 = false; //false e null são iguais
-
-const valor3 = "1";
-const valor4 = true; // 1 e true são iguais
-
-let mensagem;
-
-if (idade < 18) {
-    mensagem = "é menor de idade";
-} else if (idade == 18) {
-    mensagem = "tem exatamente 18 anos";
-}else{
-    mensagem = "é maior de idade";
-}
-
-console.log(nome, sobrenome, ultimoNome);
-
-console.log('Tipo da variavel idade', typeof idade);
-
-console.log(nome, idade, mensagem);
-
-if (idade == idade2) {
-    console.log("são iguais");
-} else {
-    console.log("não são iguais");
-}
-
-
-
+    if (info.style.display === 'none'){
+        info.style.display = "block";
+    } else {
+        info.style.display = "none";
+    }
+    
+});
