bd5a337c97cb914c82dda030157e7d51c20c9b98
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-21 22:03:40 -0300

    js test 2

--- AULA-JS/scripts/app.js
+++ AULA-JS/scripts/app.js
@@ -1,6 +1,25 @@
-console.log("Hello World, dentro do app.js");
+let nome = "johnny";
+var sobrenome = "dias mathias";  // Não utilizar var, pois pode sobrescrever variáveis
+const ultimoNome = "junior";
 
-let nome;  //Cria uma variável com valor indefinido
-let disciplina = null;  //Cria uma variável vazia
-let texto = "Este é um exemplo";  //Cria uma variável com um valor
+const idade = 18;5;
+
+let mensagem;
+
+if (idade < 18) {
+
+    mensagem = "é menor de idade";
+
+}
+else{
+
+    mensagem = "é maior de idade";
+    
+}
+
+console.log(nome, sobrenome, ultimoNome);
+
+console.log('Tipo da variavel idade', typeof idade);
+
+console.log(nome, mensagem);
 
